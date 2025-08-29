7a16e23ae63116f030c5786b028518a6600c1d5a
Kisoo Yun <yks5702@gmail.com>
2025-08-29 14:19:45 +0900

    Protect credentials

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ const express = require('express')
 const app = express()
 const port = 5001
 
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Kisoo:kisoo@boilerplate.zuk0ab8.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate').then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save()
+    .then(() => res.status(200).json({ success: true }))
+    .catch(err => res.json({ success: false, err }))
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
